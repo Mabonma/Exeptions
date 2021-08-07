14ff498ae9080c0bb5261ea9107c538bc49cfcee
Mabonma <alexey20010224@gmail.com>
2021-08-07 21:05:22 +0300

    Create Food class and menu in Main

---
+++ exeptions/src/exeption/Food.java
@@ -0,0 +1,43 @@
+package exeption;
+
+public abstract class Food {
+
+    protected String name;
+    protected int count;
+    protected double price;
+
+    public Food() {
+    }
+
+    public Food(String name, int count, double price) {
+        this.name = name;
+        this.count = count;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
+}
